f7a838cdcedac610b0b3698bff9ee1841b60bc50
lDarkHawkI <lbrainI@icloud.com>
2021-11-12 23:37:42 +0300

    X COMMIT

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,16 +2,16 @@ package ru.netology.sqr;
 
 public class SQRService {
     public int countSquare(int num, int num1) {
-        int iterationcount = 0;
+        int iterationCount = 0;
 
         for (int i = 10; i <= 99; i++)
             if (i * i >= num) {
                 if (i * i <= num1) {
-                    iterationcount++;
+                    iterationCount++;
                 }
             }
 
-        return iterationcount;
+        return iterationCount;
     }
 
 }
